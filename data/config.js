f86a664930f228e3b39b944628b395360bad85d6
jww666 <3186418859@qq.com>
2026-04-30 11:19:33 +0800

    Update map configuration with new keys and settings

--- data/config.js
+++ data/config.js
@@ -1,5 +1,6 @@
 window.PUBLIC_MAP_CONFIG = {
-  amapKey: "",
-  amapSecurityCode: "",
-  hideKeyInputs: false
+  amapKey: "b3be2d711989e6cdb117e92f4e532b16",
+  amapSecurityCode: "9e74c2977025902eba5b68965f61532e",
+  hideKeyInputs: true
 };
+
